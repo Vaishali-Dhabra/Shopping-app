6771e73cb0296166942585ed3e24edf7fd3ad650
Vaishali-Dhabra <vaishali.dhabra@catalyst.sh>
2023-04-11 18:54:27 +0530

    Added Functions

--- src/redux/action/action.js
+++ src/redux/action/action.js
@@ -4,3 +4,24 @@ export const AddToCart = (item) => {
         payload: item
     }
 }
+export const RemoveToCart = (id) => {
+    return {
+        type: 'RemoveToCart',
+        payload: id
+    }
+}
+
+export const DecreaseQuantity = (item) => {
+    return {
+        type: 'DecreaseQuantity',
+        payload: item
+    }
+}
+
+export const EmptyCart = () => {
+    return {
+        type: 'EmptyCart',
+    }
+}
+
+export default AddToCart;
